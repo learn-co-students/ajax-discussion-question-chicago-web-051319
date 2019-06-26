d91bb281ec34d65c7aad9011a546b215320ee62f
afhare <alexandra.f.hare@gmail.com>
2019-06-26 10:15:50 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,38 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+  function fetchData(){
+    fetch('https://randomuser.me/api/').then(response => response.json()).then(json => renderPerson(json));
+  };
+
+  function renderPerson(json){
+    let photo = document.getElementById('profile_picture');
+    photo.src = json.results[0].picture.medium;
+    let fullName = document.getElementById('fullname');
+    fullName.innerText = `${json.results[0].name.title} ${json.results[0].name.first} ${json.results[0].name.last}`
+    let emailAddress = document.getElementById('email');
+    emailAddress.innerText = json.results[0].email;
+    let streetAddress = document.getElementById('street');
+    streetAddress.innerText = json.results[0].location.street;
+    let cityAddress = document.getElementById('city');
+    cityAddress.innerText = json.results[0].location.city;
+    let stateAddress = document.getElementById('state');
+    stateAddress.innerText = json.results[0].location.state;
+    let zipCode = document.getElementById('postcode');
+    zipCode.innerText = json.results[0].location.postcode;
+    let phoneNumber = document.getElementById('phone');
+    phoneNumber.innerText = json.results[0].phone;
+    let cellPhone = document.getElementById('cell');
+    cellPhone.innerText = json.results[0].cell;
+    let birthday = document.getElementById('date_of_birth');
+    birthday.innerText = json.results[0].dob.date.split('T')[0];
+  };
+
+  fetchData();
+
+  const button = 
+  document.getElementsByClassName('btn-primary')[0].addEventListener('click', function(){
+    fetchData();
+  });
 });
